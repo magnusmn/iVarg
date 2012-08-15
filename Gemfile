49394bab8c91854bdff79a10d18313c5ebb856b2
Magnus Mårtensson <magnusmn@axis.com>
2012-08-15 10:40:47 +0200

    Refactored tests a bit and installed guard

--- Gemfile
+++ Gemfile
@@ -9,9 +9,15 @@ gem 'jquery-rails', '2.0.0'
 
 
 group :development, :test do
-  gem 'capybara', '1.1.2'
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.10.0'
+  gem 'guard-rspec', '0.5.5'
+end
+
+group :test do
+  gem 'capybara', '1.1.2'
+  gem 'rb-inotify', '0.8.8'
+  gem 'libnotify', '0.5.9'
 end
 
 # Gems used only for assets and not required
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,16 +3,24 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Home page" do
-    it "should have the content 'Home'" do
+    it "should have the h1 'Home'" do
       visit '/static_pages/home'
-      page.should have_content('Home')
+      page.should have_selector('h1', :text => "iVarg")
+    end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "iVarg | Home")
     end
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
+    it "should have the h1 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => "Help")
+    end
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "iVarg | Help")
     end
   end
 
