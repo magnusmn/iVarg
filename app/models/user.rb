979452b5987ed1164004a8f903ab0e283f8277cc
Magnus Martensson <magnusmn@axis.com>
2012-08-26 11:11:35 +0200

    Added some simple email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
 
-  validates :name, presence: true,
-                   length:   { maximum: 50 }
-  validates :email, presence: true
+  validates :name,  :presence => true,
+                    :length => { :maximum => 50 }
+  validates :email, :presence => true,
+                    :format => { :with => /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/,
+                    :message => "Not a valid email address" }
+ 
 
 end
