329dbeeaa13846bfc6ec6e8920001328c8287b4b
Magnus Martensson <magnus_martensson@yahoo.se>
2012-08-28 22:06:31 +0200

    Added test for signup form

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,14 +1,37 @@
 require 'spec_helper'
 
 describe "UserPages" do
+  
+  subject { page }  
+
   describe "Signup page" do
     before { visit  signup_path }
+    
+    it { should have_selector('h1', :text => "Sign up here") }
+    it { should have_selector('title', :text => "iVarg | Sign up") }
+  end
 
-    it "should have the h1 'Home'" do
-      page.should have_selector('h1', :text => "Sign up here")
+  describe "signup" do
+    before { visit  signup_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a new use" do
+        expect { click_button submit }.not_to change(User, :count)
+      end  
+    end 
+
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
     end
-    it "should have the right title" do
-      page.should have_selector('title', :text => "iVarg | Sign up")
-    end
+
   end
 end
