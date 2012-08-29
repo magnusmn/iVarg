937920011ea6e21c6a066ee9c1d2eb75a6e39ff6
Magnus Mårtensson <magnusmn@axis.com>
2012-08-29 09:49:43 +0200

    Prettyfied integration tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,12 @@ class User < ActiveRecord::Base
 
   has_many :adverts 
 
+  VALID_EMAIL_REGEX = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
+
   validates :name,  :presence => true,
-                    :length => { :maximum => 50 }
+                    :length   => { :maximum => 50 }
   validates :email, :presence => true,
-                    :format => { :with => /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/,
-                    :message => "Not a valid email address" }
- 
+                    :format   => { :with    => VALID_EMAIL_REGEX, 
+                                   :message => "Not a valid email address" }
 
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,27 +1,21 @@
 require 'spec_helper'
 
 describe "Static pages" do
+ 
+  subject { page }
 
   describe "Home page" do
     before { visit root_path }
 
-    it "should have the h1 'Home'" do
-      page.should have_selector('h1', :text => "iVarg")
-    end
-    it "should have the right title" do
-      page.should have_selector('title', :text => "iVarg | Home")
-    end
+    it { should have_selector('h1', :text => "iVarg") }
+    it { should have_selector('title', :text => "iVarg | Home") }
   end
 
   describe "Help page" do
     before { visit help_path }
 
-    it "should have the h1 'Help'" do
-      page.should have_selector('h1', :text => "Help")
-    end
-    it "should have the right title" do
-      page.should have_selector('title', :text => "iVarg | Help")
-    end
+    it { should have_selector('h1', :text => "Help") }
+    it { should have_selector('title', :text => "iVarg | Help") }
   end
 
 end
