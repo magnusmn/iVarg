08aba2308a6a3a9669b3994634149a7a5187a57b
Magnus Martensson <magnus_martensson@yahoo.se>
2013-01-04 15:56:40 +0100

    Added City and State models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130102190501) do
+ActiveRecord::Schema.define(:version => 20130104121946) do
 
   create_table "adverts", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,19 @@ ActiveRecord::Schema.define(:version => 20130102190501) do
     t.integer  "bid"
     t.integer  "price"
     t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "cities", :force => true do |t|
+    t.string   "name"
+    t.integer  "state_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "states", :force => true do |t|
+    t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8 
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "DELETING ALL ENTRIES IN THE STATE DATABASE"
+
+State.destroy_all
+City.destroy_all
+
+all_states=%w[ Blekinge Bohuslän Dalarna Dalsland Gotland Gästrikland Halland
+               Hälsingland Härjedalen Jämtland Lappland Medelpad Norrbotten
+               Närke Skåne Småland Södermanland Uppland Värmland Västerbotten
+               Västergötland Västmanland Ångermanland Öland Östergötland ]
+#id=1
+#all_states.each do |state|
+#  puts "Creating state: #{state}; ID #{id}"
+#  State.create!(id: id, name: state)
+#  id = id+1
+#end
+
+
+all_cities = [
+  { "Blekinge" => %w[ Karlskrona ] },
+  { "Bohuslän" => %w[ Göteborg ] },
+  { "Skåne"    => %w[ Malmö Lund ] },
+]
+
+all_cities.each do |a|
+  state=State.create(name: a.keys)
+  a.values.each do |city|
+    state.cities.create(name: city)
+    puts city
+  end 
+end
