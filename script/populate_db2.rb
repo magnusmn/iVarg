db4f1fc0a46fc39aa8944eedcb2762c371da80ae
Magnus Mårtensson <magnusmn@axis.com>
2013-01-13 15:06:31 +0100

    Patches to populate script

--- script/populate_db2.rb
+++ script/populate_db2.rb
@@ -5,11 +5,32 @@ require 'open-uri'
 require 'irb'
 #require "../config/environment.rb"
 
+def reset_db
+  puts "Deleting database entries"
+  User.delete_all
+  Advert.delete_all
+end
+
+def add_default_user
+  u=User.create(name: "Magnus",
+                email: "mannen@home.se",
+                password: "foobar",
+                password_confirmation: "foobar")
+
+end
+
+
+def add_admin_user
+  admin=User.new(name: "Admin",
+                 email: "admin@example.com",
+                 password: "foobar",
+                 password_confirmation: "foobar")
+  admin.toggle(:admin)
+  admin.save
+end
+
 URL="http://www.blocket.se/hela_sverige"
 PROXY="http://wwwproxy:3128"
-
-# Match 6 digits or more
-REGEX=/\d{6,}/
 
 @doc = Nokogiri::HTML(open(URL, :proxy => PROXY))
 
@@ -17,13 +38,42 @@ REGEX=/\d{6,}/
 @doc.css('div.desc a').map { |link| link['href']}.each_slice(2) { |x| @urls.push(x[0]) }
 
 
-puts "## Parsing #{@urls[0]} ##"
-@doc = Nokogiri::HTML(open(@urls[0], :proxy => PROXY))
+@url=@urls[0]
 
+puts "## Parsing #{@url} ##"
+@doc = Nokogiri::HTML(open(@url, :proxy => PROXY), nil, 'iso-8859-1')
+@doc.encoding = 'utf-8'
 
-@title=@doc.css('div.body').text.force_encoding("UTF-8").strip
+@bid=@url.match(/\d{8,}/).to_s
 
-puts @title
+if @bid.length == 8
+  @title=@doc.css('h2[class^=subject_]').text.strip
+  @body=@doc.css('div.body').text.strip
+  @bid=@bid.to_i
+  @name=@doc.css('a.contact_link').first.text
+  @price=@doc.css('table tr strong').first.text.strip.gsub(/\D/,'')
 
-#ARGV.clear
-#IRB.start
+  puts "## BID ###"
+  puts "#{@bid}"
+  puts "## TITLE ###"
+  puts "#{@title}"
+  puts "## NAME ###"
+  puts "#{@name}"
+  puts "## BODY ###"
+  puts "#{@body}"
+  puts "## PRICE ###"
+  puts "#{@price}"
+  puts "## IMAGES ###"
+
+  @doc.css('li.thumb_element a').map { |link| link['href']}.each_with_index { |x, index|
+    dest=@bid.to_s + "/" + index.to_s + ".jpg"
+    puts "Fetching image #{index} #{x} => #{dest}"
+    `mkdir -p #{@bid}`
+    `wget -v x dest`
+  }
+  #@doc.css('div#adaptive_image_size img')[0]['src']
+
+end
+
+ARGV.clear
+IRB.start
